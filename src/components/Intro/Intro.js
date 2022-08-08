27496af99fc30ca3e4c72ae2012dd5da1c26de43
Scott Andermann <Scott@Kelseys-MacBook-Air.local>
2022-08-08 15:36:39 -0400

    remove main heading animation

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -7,12 +7,12 @@ import MainHeading from './MainHeading';
 const Intro = () => {
     return (
         <div className='intro'>
-            <MainHeading text='Hello there!' />
+            {/* <MainHeading text='Hello there!' /> */}
             <motion.div
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 1 }}>
-                {/* <h1 className='main-heading'>Hello there!</h1> */}
+                <h1 className='main-heading'>Hello there!</h1>
                 <h2 className='heading-2'>I love building new things and fixing old things</h2>
                 <p className='intro-text'>Hi, my name is Scott Andermann and I took the scenic to web development. I spent the early part of my career designing off road vehicles.
                 From there I completed my MBA and transistioned into a Product Manager role for consumer goods.  I bring an endless curiosity to any new challenge
